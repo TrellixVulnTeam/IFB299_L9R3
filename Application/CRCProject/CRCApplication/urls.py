20277dbc53a195534113690f5324d86ca5678a5f
Taylor Bindon <taylor.j.bindon@gmail.com>
2018-09-21 11:25:18 +1000

    Test Signin Example

--- Application/CRCProject/CRCApplication/urls.py
+++ Application/CRCProject/CRCApplication/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
-
-from . import views
+from django.conf.urls import url
+from CRCApplication import views 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.index),
 ]
--- Application/CRCProject/CRCApplication/views.py
+++ Application/CRCProject/CRCApplication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
+from django.http import HttpResponse, HttpRequest
 
 def index(request):
-    return HttpResponse("Hello world, this is index")
+    return render(request, 'CRCApplication/signin.html')
